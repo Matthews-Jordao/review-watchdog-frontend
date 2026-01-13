ced05fd39a15dda91dc505f7e9555814588f3c8a
matthews jordao <matthewsjordao@gmail.com>
2026-01-12 19:20:16 -0500

    added responsive powered by section to hero with logos

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Hero.css';
+import searchIcon from '../../assets/images/searchicon.svg';
+import googleLogo from '../../assets/images/google-ar21.svg';
+import facebookLogo from '../../assets/images/facebook-ar21.svg';
+import yelpLogo from '../../assets/images/yelp-ar21.svg';
 
 function Hero() {
   return (
@@ -18,7 +22,18 @@ function Hero() {
               placeholder="Try 'Papa's Pizza' or 'Main Street Auto Repairs'..."
               className="hero__search-input"
             />
-            <button className="hero__search-btn">🔍</button>
+            <button className="hero__search-btn">
+              <img src={searchIcon} alt="Search" className="hero__search-icon" />
+            </button>
+          </div>
+          
+          <div className="hero__powered-by">
+            <p className="hero__powered-by-text">Powered By</p>
+            <div className="hero__powered-by-logos">
+              <img src={googleLogo} alt="Google" className="hero__powered-by-logo" />
+              <img src={facebookLogo} alt="Facebook" className="hero__powered-by-logo" />
+              <img src={yelpLogo} alt="Yelp" className="hero__powered-by-logo" />
+            </div>
           </div>
         </div>
       </div>
